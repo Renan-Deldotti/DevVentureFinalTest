59352dad15ca75b03f6aee376288db7bb16e9524
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-06-04 16:45:46 -0300

    Update
    Check connection

--- app/src/main/java/br/com/renandeldotti/devventurefinaltest/CatImageAdapter.java
+++ app/src/main/java/br/com/renandeldotti/devventurefinaltest/CatImageAdapter.java
@@ -1,7 +1,5 @@
 package br.com.renandeldotti.devventurefinaltest;
 
-import android.app.DownloadManager;
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/br/com/renandeldotti/devventurefinaltest/MainActivity.java
+++ app/src/main/java/br/com/renandeldotti/devventurefinaltest/MainActivity.java
@@ -1,6 +1,10 @@
 package br.com.renandeldotti.devventurefinaltest;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -27,23 +31,42 @@ public class MainActivity extends AppCompatActivity {
         viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
 
         imagesUrl = new ArrayList<>();
-
         adapter = new CatImageAdapter(imagesUrl);
         mainBinding.recyclerViewCats.setLayoutManager(new GridLayoutManager(this, 4));
         mainBinding.recyclerViewCats.setHasFixedSize(true);
         mainBinding.recyclerViewCats.setAdapter(adapter);
 
-        viewModel.getCatData().observe(this, cat -> {
-            if (cat != null){
-                for (Cat.Data catData : cat.getData()) {
-                    for (Cat.Images catImages : catData.getImages()){
-                        imagesUrl.add(catImages.getLink());
-                    }
-                }
-            }
-            adapter.notifyDataSetChanged();
-        });
+        mainBinding.buttonSync.setOnClickListener(v -> updateData());
+
+        updateData();
 
         setContentView(mainBinding.getRoot());
+    }
+
+    private void updateData() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected()) {
+
+            if (mainBinding.buttonSync.getVisibility() == View.VISIBLE) {
+                mainBinding.buttonSync.setVisibility(View.GONE);
+            }
+
+            viewModel.getCatData().observe(this, cat -> {
+                if (cat != null) {
+                    for (Cat.Data catData : cat.getData()) {
+                        for (Cat.Images catImages : catData.getImages()) {
+                            imagesUrl.add(catImages.getLink());
+                        }
+                    }
+                }
+                mainBinding.progressBarLoading.setVisibility(View.GONE);
+                adapter.notifyDataSetChanged();
+            });
+
+        } else {
+            mainBinding.progressBarLoading.setVisibility(View.GONE);
+            mainBinding.buttonSync.setVisibility(View.VISIBLE);
+            Toast.makeText(this, "No internet connection.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
